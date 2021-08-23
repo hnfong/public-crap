7a7fcf08c00451e2654027165d5c59865d362e79
Sidney Fong <sydneyfong@localhost>
2021-08-23 20:18:26 +0800

    Add README.md

---
+++ housekeeping/generate-readme.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+"""
+Script to generate the README.md
+"""
+
+import os
+import re
+from urllib.parse import quote as url_quote
+
+def insert(node, key_path, value):
+    if len(key_path) == 1:
+        if key_path[0] not in node:
+            node[key_path[0]] = []
+
+        node[key_path[0]].append(value)
+        return
+
+    if key_path[0] not in node:
+        node[key_path[0]] = {}
+
+    return insert(node[key_path[0]], key_path[1:], value)
+
+def capitalize_if_necessary(s):
+    if re.match(r'^[a-zA-Z]+', s):
+        return s[0].upper() + s[1:]
+    return s
+
+def preorder_dfs(node, f, d = []):
+    # print("debug", repr(node))
+
+    if hasattr(node, "append"):
+        for item in sorted(node, reverse=True):
+            preorder_dfs(item, f, d + [item,])
+    elif hasattr(node, "keys"):
+        for k in sorted(node.keys(), reverse=True):
+            f(k, d)
+            preorder_dfs(node[k], f, d + [k,])
+    else:
+        f(node, d + [None,])
+
+def p(s, d):
+    if len(d) > 0 and d[-1] is None:
+        # Leaf node
+
+        # Do nothing for the readme file.
+        if s == "README.md":
+            return
+
+        url = '/'.join(url_quote(dd.encode("utf-8")) for dd in d[:-1]) + '/' + url_quote(s.encode("utf-8"))
+        print(f"- [{s.rstrip('.md')}]({url})")
+    else:
+        print("")
+        print(("#" * (len(d)+1) ) + " " + capitalize_if_necessary(s))
+        print("")
+
+root_node = {}
+
+for root, directories, files in os.walk("."):
+    for fn in files:
+        if "/." in root: continue
+        if fn.endswith(".md"):
+            insert(root_node, root.lstrip("./").split("/"), fn)
+
+preorder_dfs(root_node, p)
