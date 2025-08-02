c21723e2e1dcfc969fa8447c2a5a50ea4ce8e37c
Sidney Fong <sydneyfong@localhost>
2025-08-03 02:43:52 +0800

    Fix the eval for code-009

--- prompts/test009main.py
+++ prompts/test009main.py
@@ -2,19 +2,19 @@ import sys
 
 CASES = [
             {  # 0
-                "args": ("code/009-code2.test.1.in", "0"),
+                "args": ("code/009-code2.test.1.in", b"0"),
                 "expect": 0,
             },
             {  # 1
-                "args": ("code/009-code2.test.1.in", "9"),
+                "args": ("code/009-code2.test.1.in", b"9"),
                 "expect": 127,
             },
             {  # 2
-                "args": ("code/009-code2.test.1.in", "2 by"),
+                "args": ("code/009-code2.test.1.in", b"2 by"),
                 "expect": 17,
             },
             {  # 3
-                "args": ("code/009-code2.test.1.in", "1 Z"),
+                "args": ("code/009-code2.test.1.in", b"1 Z"),
                 "expect": 17,
             },
         ]
