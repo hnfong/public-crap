1f16c1951873d8cf2403936b7b00ade2a93cd6e4
Sidney Fong <sydneyfong@localhost>
2025-08-03 00:04:52 +0800

    Evaluation for file bisection.
    Rewrote the prompt to be less ambiguous.

---
+++ prompts/test009main.py
@@ -0,0 +1,27 @@
+import sys
+
+CASES = [
+            {  # 0
+                "args": ("code/009-code2.test.1.in", "0"),
+                "expect": 0,
+            },
+            {  # 1
+                "args": ("code/009-code2.test.1.in", "9"),
+                "expect": 127,
+            },
+            {  # 2
+                "args": ("code/009-code2.test.1.in", "2 by"),
+                "expect": 17,
+            },
+            {  # 3
+                "args": ("code/009-code2.test.1.in", "1 Z"),
+                "expect": 17,
+            },
+        ]
+
+import test009
+
+case = CASES[int(sys.argv[1])]
+result = test009.binary_search_file(*case["args"])
+print(repr(result), " vs ", repr(case["expect"]))
+assert result == case["expect"]
